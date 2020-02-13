b5d073cd71935bd24285f7d7bc89ddea518d72d9
Marcelo Medeiros <marcelolinsdemedeiros@gmail.com>
2020-02-13 12:10:53 -0200

    BoardingException and defensive programing

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,8 +4,7 @@ import chess.ChassMatch;
 
 public class Program {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		ChassMatch chassMatch = new ChassMatch();
 		UI.printBoard(chassMatch.getPieces()); // chama o método e coloca as peças
 		
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import java.util.prefs.BackingStoreException;
+
 public class Board {
 	
 	private int rows;
@@ -7,6 +9,9 @@ public class Board {
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1)  {
+			throw new BoardException("Errror creating Board: row > 1, column > 1");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns]; 
@@ -14,35 +19,53 @@ public class Board {
 
 	public int getRows() {
 		return rows;
-	}
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+	}	
 
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	//  cria o tamanho  da matriz peça
 	// define o length da peça sobre o tabuleito
 	public Piece piece(int row , int column){
+		if (!positionExists (row , column)){
+			throw new BoardException("Position not on the board");
+		}
 		return pieces [row][column];
 	}
 	// seta, define a posição da peça
 	public Piece piece(Position position){
+		if (!positionExists (position)) {
+			throw new BoardException("Position not on the board");	
+		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	// aloca peça no tabuleiro
 	public void placePiece(Piece piece, Position position){
+		if (thereIsSpiece(position)) {
+			throw new BoardException(" there is already a piece on position"+position); //BackingStoreException
+		}
 		// bote essa peça aqui, nessa linha e coluna, essa linha e
 		// essa linha e essa coluna rece essa peça
 		pieces[position.getRow()][position.getColumn()] = piece; 
 		piece.position = position;
 	}
+	
+	public boolean positionExists(int row, int column){
+		return row >= 0 && row < rows && column >= 0 && column < columns ;
+	}	
+	
+	public Boolean positionExists(Position position){
+		return positionExists(position.getRow(),position.getColumn());
+	}
+	
+	public boolean thereIsSpiece(Position position){
+		if (!positionExists(position)) {
+			throw new BoardException(" there is already a piece on position"+position); //BackingStoreException
+		}
+		return piece (position) !=null;
+	}
+	
 	
 	
 	
